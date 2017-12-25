d7a3d0978439f90927504448b420462bc2651479
Primož Pečar <pp1508@student.uni-lj.si>
2017-12-25 12:44:56 +0100

    Update kernel.c
    change encoding

--- VPSA_SEMINARSKA_PARALEL/kernel.c
+++ VPSA_SEMINARSKA_PARALEL/kernel.c
@@ -1,3 +1,4 @@
+//Testing on linux
 #define NUM_THREADS 4
 static inline int returnMinimumValue(int a, int b, int c);
 static inline int returnMinimumValue(int a, int b, int c) {
@@ -24,7 +25,7 @@ __kernel void seamCarving(__global int *slikaInput, int InputW, int InputH)
 	for (int i = h - 2; i >= 0; i--) {
 		for (int j = 0; j < vector_length_for_thread; j++) {
 			//gremo po celotni vrstici
-			//trenutna pozicija glede na širino in stolpec v katerem smo plus trenutni indeks 
+			//trenutna pozicija glede na Å¡irino in stolpec v katerem smo plus trenutni indeks 
 			int trenutna_pozicija_v_vektorju = (w / NUM_THREADS*i) + j + (get_global_id(0)*vector_length_for_thread);
 			int trenutna_vrednost_v_vektorju = slikaInput[trenutna_pozicija_v_vektorju];
 			left = slikaInput[trenutna_pozicija_v_vektorju + (w - 1)];
@@ -32,7 +33,7 @@ __kernel void seamCarving(__global int *slikaInput, int InputW, int InputH)
 			right = slikaInput[trenutna_pozicija_v_vektorju + (w + 1)];
 			//ROBNI PRIMER 1, CE SEM NA ZACETKU NE PREVERJAM LEVEGA
 			if (j == 0) {
-				//ÈE JE MOJ SPODNJI SOSED DIREKTNO POD MANO MANJŠI OD MOJEGA DESNEGA SOSEDA GA PRIŠTEJEM
+				//ÃˆE JE MOJ SPODNJI SOSED DIREKTNO POD MANO MANJÅ I OD MOJEGA DESNEGA SOSEDA GA PRIÅ TEJEM
 				if (middle < right) {
 					trenutna_vrednost_v_vektorju += middle;
 				}
